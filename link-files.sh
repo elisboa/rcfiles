d366a85ead661b74349a91ac451383eed0039bdf
Tmgit Script <tmgit@localhost>
2020-06-30 20:26:01 -0300

    link-files.sh Automated commit at 2020.06.30-20.26

--- link-files.sh
+++ link-files.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -x
 
-ln -f ~/.tmux.conf $(dirname $0)
-ln -f ~/.vimrc $(dirname $0)
+ln -f ~/.tmux.conf $(dirname $0)/.tmux.conf
+ln -f ~/.vimrc $(dirname $0)/.vimrc
 ln -f ~/.ssh/config $(dirname ${0})/ssh/config
 ln -f ~/.i3/config $(dirname ${0})/i3/config
+ln -f ~/.config/i3status/config $(dirname ${0})/i3status/config
 
