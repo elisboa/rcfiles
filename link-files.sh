b2a528858c6c7e12e99633e4658f310151adba09
Tmgit Script <tmgit@localhost>
2020-07-01 17:15:01 -0300

    link-files.sh Automated commit at 2020.07.01-17.15

--- link-files.sh
+++ link-files.sh
@@ -8,6 +8,6 @@ ln -f ~/.config/i3status/config $(dirname ${0})/i3status/config
 
 for file in ~/Documentos/Cool\ Retro\ Term/*
 do
-  echo ln -f "${file}" "$(dirname ${0})"/"$(${file})"
-  ln -f "${file}" "$(dirname ${0})"/"$(${file})"
+  echo ln -f "${file}"
+  ln -f "${file}" 
 done
